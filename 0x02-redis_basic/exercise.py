df010bd2fe8d85912f5db2fc195ed9b7bc8a6e5f
Ato Toffah <atotoffah@gmail.com>
2024-03-27 21:55:31 +0000

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-from typing import Union
-from uuid import uuid4
-
-import redis
-
 """
 Contains the Cache class definition
 """
+from typing import Callable, Union
+from uuid import uuid4
+
+import redis
 
 
 class Cache:
@@ -25,3 +24,24 @@ class Cache:
         name = str(uuid4())
         self._redis.set(name, data)
         return name
+
+    def get(
+        self, key: str, fn: Callable = None
+    ) -> Union[str, bytes, int, float]:
+        """
+        Gets a value by its key from Redis db
+        Converts the value if callable fn is not none
+        """
+        val = self._redis.get(key)
+        if fn is not None and val is not None:
+            val = fn(val)
+
+        return val
+
+    def get_str(self, key: str) -> str:
+        """Gets a value from redis and returns it as str"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Gets a value from redis and returns it as int"""
+        return self.get(key, int)
